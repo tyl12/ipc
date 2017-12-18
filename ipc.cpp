1a0b30df98bea7cb3667e0a44b1ac94669e0d502
Teng Yiliang <yiliang.teng@weimob.com>
2017-12-18 12:52:49 +0800

    use nmap instead.
    Signed-off-by: Teng Yiliang <yiliang.teng@weimob.com>

--- ipc.cpp
+++ ipc.cpp
@@ -6,12 +6,16 @@
 #include <stdio.h>
 #include <iostream>
 #include <regex>
+#include <mutex>
 #include <thread>
+#include <algorithm>
 
 using namespace cv;
 using namespace std;
 #define MAXLINE 1024
-#define DEBUG false
+#define DEBUG true
+
+mutex m;
 
 int main(int argc, char** argv)
 {
@@ -19,10 +23,48 @@ int main(int argc, char** argv)
 
 	vector<string> mac_table;
 	mac_table.push_back("b4:a3:82:5e:5f:15");
+	mac_table.push_back("4c:bd:8f:c6:30:1f");
 
 	char result_buf[MAXLINE];
 	vector<tuple<string,string>> mac_ip_table;
 
+    cout<<endl<<"Start to scan local device ip-mac tables....."<<endl;
+
+#if 1
+	FILE* fp = popen("sudo nmap -sP  192.168.10.0/24 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print \" => \"$3;}' | sort", "r");
+	if(NULL == fp)
+	{
+		cerr<<"fail to execute arp -a"<<endl;
+		exit(1);
+	}
+	while(fgets(result_buf, sizeof(result_buf), fp) != NULL)
+	{
+		int len=strlen(result_buf);
+		if (result_buf[len-1] == '\n'){
+			result_buf[len-1]=0;
+		}
+		if (DEBUG){
+			cout<<result_buf<<endl;
+		}
+		string str=result_buf;
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
+
+		for (auto mac:mac_table){
+			regex re(string("([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+).*=>.*")+mac+".*");
+			smatch sm;
+			regex_match(str, sm, re);
+			if (DEBUG){
+				for (auto s:sm){
+					cout<<"sm: " << s<<endl;
+				}
+			}
+			if (sm.size() == 2){
+				cout<<"ip="<<sm[1]<<" mac="<<mac<<endl;
+				mac_ip_table.push_back(tuple<string,string>(sm[1], mac));
+			}
+		}
+	}
+#if 0
 	FILE* fp = popen("arp -a", "r");
 	if(NULL == fp)
 	{
@@ -54,6 +96,22 @@ int main(int argc, char** argv)
 			}
 		}
 	}
+#endif
+#else
+    mac_ip_table.push_back(make_tuple("192.168.10.180","b4:a3:82:5e:5f:15"));
+    mac_ip_table.push_back(make_tuple("192.168.10.138","4c:bd:8f:c6:30:1f"));
+#endif
+
+    cout<<"Scan Done."<<endl<<endl;
+
+    cout<<"Found ip - map mapping:"<<endl;
+	for (auto s:mac_ip_table){
+		string ip;
+		string mac;
+		tie(ip,mac)=s;
+        cout<<"    "<<ip<<" => "<<mac<<endl;
+    }
+    cout<<endl;
 
 
 	vector<thread> ts;
@@ -62,7 +120,7 @@ int main(int argc, char** argv)
 		string mac;
 		tie(ip,mac)=s;
 
-		string rtsp= string("rtsp://admin:xxxxxxxxx@" + ip +":554/ch0/main/av_stream");
+		string rtsp= string("rtsp://admin:xxxxxxx@" + ip +":554/ch0/main/av_stream");
 		ts.push_back(thread(
 			[=](){
 				VideoCapture sequence(rtsp);
@@ -82,8 +140,11 @@ int main(int argc, char** argv)
 						break;
 					}
 
-					imshow(ip, image);
-					waitKey(1);
+                    {
+                        lock_guard<mutex> lock(m);
+                        imshow(ip, image);
+                        waitKey(1);
+                    }
 				}
 			}
 		));
