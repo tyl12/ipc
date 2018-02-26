fdb7ebe252a108b5a7658b2cb7c1b9951f167bd6
Teng Yiliang <ylteng@outlook.com>
2018-02-26 11:18:14 +0800

    add some optional cv lib for ubuntu16.04
    Signed-off-by: Teng Yiliang <ylteng@outlook.com>

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 
 function check_package()
 {
+    #"libcv2.4  libhighgui2.4  libhighgui-dev libcv-dev  libopencv-contrib-dev libopencv-core-dev libopencv-core2.4v5 libopencv-dev libopencv-gpu-dev libopencv-gpu2.4v5 libopencv-imgproc-dev libopencv-video-dev  libopencv-video2.4v5 libopencv2.4-java libopencv2.4-jni"
     check_list="nmap libboost-all-dev"
     for s in $check_list; do
         apt list --installed $s 2>&1 |grep $s > /dev/null
